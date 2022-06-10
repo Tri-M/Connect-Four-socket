f4055953d3edd88405479a87c819e0ea8016ee77
Tri-M <trisham0910@gmail.com>
2022-06-10 21:26:39 +0530

    display done

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
 def printBoard(board):
-    board = [x for y in board for x in y]
+    board = [i for j in board for i in j]
+
     return("{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n".format(chr(9312), chr(9313), chr(9314), chr(9315), chr(9316), chr(9317), chr(9318), *board))

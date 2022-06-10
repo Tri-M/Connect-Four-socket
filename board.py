1a2ab30975a90a170d906db96ef727848b2f1d3b
Tri-M <trisham0910@gmail.com>
2022-06-10 13:37:47 +0530

    board.py done

---
+++ board.py
@@ -0,0 +1,9 @@
+def play(board,col,player):
+    if col<1 and col>11:
+        return None
+    else:
+        for row in range(9,-1,-1):
+            if board[row][col-1]==0:
+                board[row][col-1] = player
+                return board
+    return None

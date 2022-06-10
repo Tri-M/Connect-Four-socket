cc70d4f2afe312f128c4e6d238bc282bc60d01dd
Tri-M <trisham0910@gmail.com>
2022-06-10 21:28:42 +0530

    clients almost done

--- client1.py
+++ client1.py
@@ -5,17 +5,17 @@ port = 65432
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
-name = input("Enter your playing character: ")[0].encode('UTF-8')
+name = input("Enter your playing character : ")[0].encode('UTF-8')
 
-from_server = client.recv(4096)
-print("\n"+from_server.decode('UTF-8'))
+receive_from_server = client.recv(4096)
+print("\n"+receive_from_server.decode('UTF-8'))
 client.send(b"hi$client1$" + name)
 
 
 def selfTurn():
     print("\nYour turn.\nBoard:\n")
-    from_server = client.recv(4096)
-    print(from_server.decode('UTF-8'))
+    receive_from_server = client.recv(4096)
+    print(receive_from_server.decode('UTF-8'))
     while True:
         move = input("Enter column number to drop and start connecting !!!: ")
         if(int(move) >= 1 and int(move) <= 7):
@@ -26,30 +26,30 @@ def selfTurn():
 
 def opponentTurn():
     print("\nOpponent's turn. Kindly Wait !!! \nBoard:\n")
-    from_server = client.recv(4096)
-    print(from_server.decode('UTF-8'))
+    receive_from_server = client.recv(4096)
+    print(receive_from_server.decode('UTF-8'))
     client.send(b"$success")
 
 
 i = 0
 prev_state = "$p2"
-from_server = client.recv(4096)
+receive_from_server = client.recv(4096)
 while i <= 42:
-    if(from_server.decode('UTF-8') == "$p1" and prev_state == "$p2"):
+    if(receive_from_server.decode('UTF-8') == "$p1" and prev_state == "$p2"):
         i += 1
         prev_state = "$p1"
         selfTurn()
-        from_server = client.recv(4096)
-    elif(from_server.decode('UTF-8') == "$p2" and prev_state == "$p1"):
+        receive_from_server = client.recv(4096)
+    elif(receive_from_server.decode('UTF-8') == "$p2" and prev_state == "$p1"):
         i += 1
         prev_state = "$p2"
         opponentTurn()
-        from_server = client.recv(4096)
-    if(from_server.decode('UTF-8') == "$end"):
+        receive_from_server = client.recv(4096)
+    if(receive_from_server.decode('UTF-8') == "$end"):
         break
 
 client.send(b"$success")
-from_server = client.recv(4096)
-print(from_server.decode('UTF-8'))
+receive_from_server = client.recv(4096)
+print(receive_from_server.decode('UTF-8'))
 
 end = input("Press Enter key to close.")
--- client2.py
+++ client2.py
@@ -7,15 +7,15 @@ client.connect((host, port))
 
 name = input("\nEnter your playing character: ")[0].encode('UTF-8')
 
-from_server = client.recv(4096)
-print("\n"+from_server.decode('UTF-8'))
+receive_from_server = client.recv(4096)
+print("\n"+receive_from_server.decode('UTF-8'))
 client.send(b"hi$client2$" + name)
 
 
 def selfTurn():
     print("\nYour turn.\nBoard:\n")
-    from_server = client.recv(4096)
-    print(from_server.decode('UTF-8'))
+    receive_from_server = client.recv(4096)
+    print(receive_from_server.decode('UTF-8'))
     while True:
         move = input("Enter column number to drop and start connecting !!! : ")
         if(int(move) >= 1 and int(move) <= 7):
@@ -26,31 +26,31 @@ def selfTurn():
 
 def opponentTurn():
     print("\nOpponent's turn. Kindly Wait !!! \nBoard:\n")
-    from_server = client.recv(4096)
-    print(from_server.decode('UTF-8'))
+    receive_from_server = client.recv(4096)
+    print(receive_from_server.decode('UTF-8'))
     client.send(b"$success")
 
 
 i = 0
 prev_state = "$p2"
-from_server = client.recv(4096)
+receive_from_server = client.recv(4096)
 while i <= 42:
-    if(from_server.decode('UTF-8') == "$p2" and prev_state == "$p1"):
+    if(receive_from_server.decode('UTF-8') == "$p2" and prev_state == "$p1"):
         i += 1
         prev_state = "$p2"
         selfTurn()
-        from_server = client.recv(4096)
+        receive_from_server = client.recv(4096)
 
-    elif(from_server.decode('UTF-8') == "$p1" and prev_state == "$p2"):
+    elif(receive_from_server.decode('UTF-8') == "$p1" and prev_state == "$p2"):
         i += 1
         prev_state = "$p1"
         opponentTurn()
-        from_server = client.recv(4096)
-    if(from_server.decode('UTF-8') == "$end"):
+        receive_from_server = client.recv(4096)
+    if(receive_from_server.decode('UTF-8') == "$end"):
         break
 
 client.send(b"$success")
-from_server = client.recv(4096)
-print(from_server.decode('UTF-8'))
+receive_from_server = client.recv(4096)
+print(receive_from_server.decode('UTF-8'))
 
 end = input("Press Enter key to close.")
